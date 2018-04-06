0c5f5409021de592284abbb11182608ca2b89b15
Sean Halupczynski <sean.halup@gmail.com>
2018-04-06 10:16:26 -0400

    FINISHEDgit add -Agit add -A!

--- CrystalCollector/assets/javascript/crystalcollectorgame.js
+++ CrystalCollector/assets/javascript/crystalcollectorgame.js
@@ -18,101 +18,48 @@
     document.getElementById("loss").innerHTML = "Losses: " + scoreboard.loss;
     document.getElementById("randomNumber").innerHTML = values.targetValue;
 
-    // Adding values to the user's total
-    // while (values.targetValue >= userScore) {
-        // if (values.targetValue > userScore) {
-                
     var userScore = 0;
 
+    $("#scoreTotal").html(userScore);
+
+
+    function resetGame() {
+            values.targetValue = Math.floor(Math.random() * (101 + 1)) + 19;
+            values.rubyValue = Math.floor(Math.random() * 12) + 1;
+            values.diamondValue = Math.floor(Math.random() * 12) + 1;
+            values.citrineValue = Math.floor(Math.random() * 12) + 1;
+            values.emeraldValue = Math.floor(Math.random() * 12) + 1;
+            userScore = 0;
+            document.getElementById("randomNumber").innerHTML = values.targetValue;
+            $("#scoreTotal").html(userScore);
+
+    }
+
     $("#ruby, #diamond, #citrine, #emerald").click(function() {
+        
         //Click the ruby button and add it to the score 
         if(this.id == "ruby") {
             console.log(values.rubyValue);
             userScore += values.rubyValue;
             console.log(userScore);
-            // Alert win and ask to play again
-            // if (values.targetValue === userScore) {
-            //     alert("YOU WON!!!!");
-            //     var playAgainWin = confirm("Would you like to play again?");
-            // };
-            // // Alert loss and ask to play again
-            // if (values.targetValue < userScore) {
-            //     alert("You loss.");
-            //     var playAgainLoss = confirm("Would you like to play again?")
-            // };
-
-            // if (playAgainWin = true) {
-            //     $("#wins").text(scoreboard.win++);
-            // };
-
-            // if (playAgainLoss = true) {
-            //     $("#loss").text(scoreboard.loss++);
-            // };
         }
         // Click the diamond and add it to the score
         else if(this.id == "diamond") {
             console.log(values.diamondValue);
             userScore += values.diamondValue;
             console.log(userScore);
-            // // Alert win and ask to play again
-            // if (values.targetValue === userScore) {
-            //     alert("YOU WON!!!!");
-            //     var playAgainWin = confirm("Would you like to play again?");
-            // };
-            // // Alert loss and ask to play again
-            // if (values.targetValue < userScore) {
-            //     alert("You loss.");
-            //     var playAgainLoss = confirm("Would you like to play again?")
-            // };
-
-            // if (playAgainWin = true) {
-            //     $("#wins").text(scoreboard.win++);
-            // };
-
-            // if (playAgainLoss = true) {
-            //     $("#loss").text(scoreboard.loss++);
-            // };
         }
         // Click the citrine button and add it to the score
         else if(this.id == "citrine") {
             console.log(values.citrineValue);
             userScore += values.citrineValue;
             console.log(userScore);
-            // // Alert win and ask to play again
-            // if (values.targetValue === userScore) {
-            //     alert("YOU WON!!!!");
-            //     var playAgainWin = confirm("Would you like to play again?");
-            // };
-            // // Alert loss and ask to play again
-            // if (values.targetValue < userScore) {
-            //     alert("You loss.");
-            //     var playAgainLoss = confirm("Would you like to play again?")
-            // };
-
-            // if (playAgainWin = true) {
-            //     $("#wins").text(scoreboard.win++);
-            // };
-
-            // if (playAgainLoss = true) {
-            //     $("#loss").text(scoreboard.loss++);
-            // };
         }
         // Click the emerald button and add it to the score
         else if(this.id == "emerald") {
             console.log(values.emeraldValue);
             userScore += values.emeraldValue;
             console.log(userScore);
-            // // Alert win and ask to play again
-            // if (values.targetValue === userScore) {
-            //     alert("YOU WON!!!!");
-            //     var playAgainWin = confirm("Would you like to play again?");
-            // };
-            // // Alert loss and ask to play again
-            // if (values.targetValue < userScore) {
-            //     alert("You loss.");
-            //     var playAgainLoss = confirm("Would you like to play again?")
-            // };
-
         };
 
         $("#scoreTotal").text(userScore);
@@ -122,17 +69,27 @@
             alert("YOU WON!!!!");
             var playAgainWin = confirm("Would you like to play again?");
             // Incrementing wins
-            if (playAgainWin === true) {
-                $("#wins").text("Wins: " + scoreboard.win++);
+            if (playAgainWin == true) {
+                scoreboard.win++;
+                $("#wins").text("Wins: " + scoreboard.win);
+                resetGame();
+            }
+            else {
+                location.reload();
             };
         };
         // Alert loss and ask to play again
         if (values.targetValue < userScore) {
             alert("You loss.");
-            var playAgainLoss = confirm("Would you like to play again?")
+            var playAgainLoss = confirm("Would you like to play again?");
             // Incrementing losses
-            if (playAgainLoss === true) {
-                $("#loss").text("Losses: " + scoreboard.loss++);
-            };
+            if (playAgainLoss == true) {
+                scoreboard.loss++;
+                $("#loss").text("Losses: " + scoreboard.loss);
+                resetGame(values.targetValue, values.rubyValue, values.diamondValue, values.citrineValue, values.emeraldValue, userScore);
+            }
+            else {
+                location.reload();
+            };  
         };
     });
